7d14bb3a3ab0f9ed7f95482e21358b1b11b00fc4
Fer Zurita <ferzurita@Fers-MacBook-Air.local>
2018-04-30 13:46:49 -0500

    Se agregaron las funciones que se utilizaran para el modulo de "ruteo.c"

---
+++ ruteo.h
@@ -0,0 +1,5 @@
+void tablasRuteo(nodo **inicio, int **tablaA);
+int conNodos(nodo *inicio);
+int Comprobado(rtabla tabla[], int nNodos);
+void iniciarTablas(rtabla tabla[], int tabla1[],int nNodos);
+void imprimir(nodo *inicio);
